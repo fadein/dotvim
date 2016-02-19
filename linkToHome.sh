16e1dac558c8dedcec8aaa2916d1885e29ff61fa
Erik Falor (APG) <erikf@apgsensors.com>
2016-02-19 16:22:53 -0700

    add linkToHome.sh to install .vim symlink

---
+++ linkToHome.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# When the environment variable DRYRUN is non-empty, do not
+# actually make any changes, but only show what would be done
+
+set -e
+
+echodo() {
+	echo $@
+	[ 0"$DRYRUN" = "0" ] && $@ || true
+}
+
+linkToHome() {
+	if [ $# -lt 2 ]; then
+		echo Usage: $0 SOURCE_NAME DEST_NAME
+	else
+		SOURCE_NAME=$1
+		DEST_NAME=$HOME/$2
+
+		if [ -h   $DEST_NAME ]; then
+			if [ "$(readlink $DEST_NAME)" != "$SOURCE_NAME" ]; then
+				echo "$DEST_NAME is already a symlink which doesn't point here"
+			fi
+
+		elif [ -d $DEST_NAME ]; then
+			echo  $DEST_NAME already exists and is a directory
+
+		elif [ -b $DEST_NAME ]; then
+			echo  $DEST_NAME already exists as a block special
+
+		elif [ -c $DEST_NAME ]; then
+			echo  $DEST_NAME already exists as a character special
+
+		elif [ -p $DEST_NAME ]; then
+			echo  $DEST_NAME already exists as a named pipe
+
+		elif [ -S $DEST_NAME ]; then
+			echo  $DEST_NAME already exists as a socket
+
+		elif [ -f $DEST_NAME ]; then
+			echo  $DEST_NAME already exists as a regular file
+
+		elif [ -e $DEST_NAME ]; then
+			echo  $DEST_NAME already exists
+		else
+			echodo ln -s $SOURCE_NAME $DEST_NAME
+		fi
+	fi
+}
+
+# make sure that $HOME is defined
+if [ 0"$HOME" = "0" ]; then
+	echo "HOME is empty or unset!"
+	exit 1
+fi
+
+# Resolve the location of this script
+HERE=$(dirname $(readlink -f $0))
+
+if [ 0"$DRYRUN" != "0" ]; then
+	echo ====================
+	echo THIS IS A DRY RUN!!!
+	echo ====================
+	echo
+fi
+
+# Link these files and directories into $HOME
+linkToHome $HERE               .vim
